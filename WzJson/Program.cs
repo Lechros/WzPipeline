12f5cec2d7d0c136924ffdeffc0653fc1bb8fd38
OniOniOn <0onionion0@gmail.com>
2023-04-23 17:05:10 +0900

    refactor: refactor program to not change index everytime

--- WzJson/Program.cs
+++ WzJson/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 using System.Diagnostics;
+using System.Reflection.Metadata;
 using WzJson.Gear;
 using WzJson.SetItem;
 using WzJson.SimapleGear;
@@ -18,99 +19,103 @@ wz.Load(@"C:\Nexon\Maple");
 sw.Stop();
 Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
 
+List<(string, Action)> options = new()
+{
+    ("export gear data", () =>
+    {
+        Console.WriteLine("Loading gear data...");
+        sw.Restart();
+        GearLoader gl = new(wz);
+        gl.Load();
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+
+        Console.WriteLine("Saving to file...");
+        sw.Restart();
+        gl.Save(Path.Join(outputRoot, @"output\gear.json"));
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+    }
+    ),
+    ("export item option data", () =>
+    {
+        Console.WriteLine("Loading item option data...");
+        sw.Restart();
+        ItemOptionLoader ol = new(wz);
+        ol.Load();
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+
+        Console.WriteLine("Saving to file...");
+        sw.Restart();
+        ol.Save(Path.Join(outputRoot, @"output\itemoption.json"));
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+    }
+    ),
+    ("export set item data", () =>
+    {
+        Console.WriteLine("Loading set item data...");
+        sw.Restart();
+        SetItemLoader sl = new(wz);
+        sl.Load();
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+
+        Console.WriteLine("Saving to file...");
+        sw.Restart();
+        sl.Save(Path.Join(outputRoot, @"output\setitem.json"));
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+    }
+    ),
+    ("export soul data", () =>
+    {
+        Console.WriteLine("Loading soul data...");
+        sw.Restart();
+        SoulLoader sl = new(wz);
+        sl.Load();
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+
+        Console.WriteLine("Saving to file...");
+        sw.Restart();
+        sl.Save(Path.Join(outputRoot, @"output\soul.json"));
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+    }
+    ),
+    ("[simaple] export gear data", () =>
+    {
+        Console.WriteLine("Loading gear data...");
+        sw.Restart();
+        SimapleGearLoader gl = new(wz);
+        gl.Load();
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+
+        Console.WriteLine("Saving to file...");
+        sw.Restart();
+        gl.Save(Path.Join(outputRoot, @"output\simaple-gear.json"));
+        sw.Stop();
+        Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
+    }
+    ),
+};
+
 while(!exitFlag)
 {
     Console.WriteLine("----------\nChoose option:");
-    Console.WriteLine("1: export gear data");
-    Console.WriteLine("2: export item option data");
-    Console.WriteLine("3: export set item data");
-    Console.WriteLine("4: export soul data");
-    Console.WriteLine("5: [simaple] export gear data");
-    Console.WriteLine("6: exit");
-    switch(Console.ReadLine())
+    for(int i = 0; i < options.Count; i++)
     {
-        case "1":
-            {
-                Console.WriteLine("Loading gear data...");
-                sw.Restart();
-                GearLoader gl = new(wz);
-                gl.Load();
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-
-                Console.WriteLine("Saving to file...");
-                sw.Restart();
-                gl.Save(Path.Join(outputRoot, @"output\gear.json"));
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-            }
-            break;
-        case "2":
-            {
-                Console.WriteLine("Loading item option data...");
-                sw.Restart();
-                ItemOptionLoader ol = new(wz);
-                ol.Load();
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-
-                Console.WriteLine("Saving to file...");
-                sw.Restart();
-                ol.Save(Path.Join(outputRoot, @"output\itemoption.json"));
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-            }
-            break;
-        case "3":
-            {
-                Console.WriteLine("Loading set item data...");
-                sw.Restart();
-                SetItemLoader sl = new(wz);
-                sl.Load();
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-
-                Console.WriteLine("Saving to file...");
-                sw.Restart();
-                sl.Save(Path.Join(outputRoot, @"output\setitem.json"));
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-            }
-            break;
-        case "4":
-            {
-                Console.WriteLine("Loading soul data...");
-                sw.Restart();
-                SoulLoader sl = new(wz);
-                sl.Load();
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-
-                Console.WriteLine("Saving to file...");
-                sw.Restart();
-                sl.Save(Path.Join(outputRoot, @"output\soul.json"));
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-            }
-            break;
-        case "5":
-            {
-                Console.WriteLine("Loading gear data...");
-                sw.Restart();
-                SimapleGearLoader gl = new(wz);
-                gl.Load();
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-
-                Console.WriteLine("Saving to file...");
-                sw.Restart();
-                gl.Save(Path.Join(outputRoot, @"output\simaple-gear.json"));
-                sw.Stop();
-                Console.WriteLine("Done!" + $" ({sw.ElapsedMilliseconds}ms)");
-            }
-            break;
-        case "6":
-            exitFlag = true;
-            break;
+        Console.WriteLine((i + 1) + ". " + options[i].Item1);
+    }
+    if(int.TryParse(Console.ReadLine(), out int input) && (uint)(input - 1) < options.Count)
+    {
+        options[input - 1].Item2();
+    }
+    else
+    {
+        exitFlag = true;
     }
 }
