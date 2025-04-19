c1069a5b144f61183b520f7749c3837c03e50ccd
Lechros <0onionion0@gmail.com>
2025-04-19 22:03:52 +0900

    feat: simplify gear data conversion

--- WzJson/Converter/GearConverter.cs
+++ WzJson/Converter/GearConverter.cs
@@ -27,10 +27,9 @@ public class GearConverter(
         var gearId = int.Parse(key);
         var gear = new Gear
         {
-            Meta = { Id = gearId },
+            Id = gearId,
             Name = gearString.Name,
             Desc = gearString.Desc,
-            Type = GetGearType(gearId)
         };
         foreach (var propNode in infoNode.Nodes)
         {
@@ -46,115 +45,35 @@ public class GearConverter(
                 case "option":
                     gear.Potentials = ConvertToGearPotentials(propNode);
                     break;
+                case "onlyUpgrade":
+                    gear.Props[GearPropType.onlyUpgrade.ToString()] = 1;
+                    break;
                 default:
                     HandlePropNodeDefault(gear, propNode);
                     break;
             }
         }
 
-        if (gear.Attributes._SharableOnce && gear.Attributes.Share == GearAttribute.GearShare.AccountSharable)
-            gear.Attributes.Share = GearAttribute.GearShare.AccountSharableOnce;
-
-        if (gear.Potentials != null && gear.Potentials.Any(p => p != null) &&
-            gear.PotentialGrade == PotentialGrade.Normal)
-            gear.PotentialGrade = PotentialGrade.Rare;
-
-        gear.MaxStar = GetGearMaxStar(gear);
-
         return gear;
     }
 
-    private GearType GetGearType(int code)
+    private GearPotential[] ConvertToGearPotentials(Wz_Node propNode)
     {
-        switch (code / 1000)
+        var potentials = new List<GearPotential>(3);
+        foreach (var optionNode in propNode.Nodes)
         {
-            case 1098:
-                return GearType.soulShield;
-            case 1099:
-                return GearType.demonShield;
-            case 1212:
-                return GearType.shiningRod;
-            case 1213:
-                return GearType.tuner;
-            case 1214:
-                return GearType.breathShooter;
-            case 1404:
-                return GearType.chakram;
-        }
-
-        if (code / 10000 == 135)
-        {
-            switch (code / 100)
-            {
-                case 13522:
-                case 13528:
-                case 13529:
-                case 13540:
-                    return (GearType)(code / 10);
-
-                default:
-                    return (GearType)(code / 100 * 10);
-            }
-        }
-
-        if (code / 10000 == 119)
-        {
-            switch (code / 100)
-            {
-                case 11902:
-                    return (GearType)(code / 10);
-            }
-        }
-
-        return (GearType)(code / 10000);
-    }
-
-    private static readonly int[][] MaxStarData =
-    [
-        [0, 5, 3],
-        [95, 8, 5],
-        [110, 10, 8],
-        [120, 15, 10],
-        [130, 20, 12],
-        [140, 25, 15]
-    ];
-
-    private int GetGearMaxStar(Gear gear)
-    {
-        if (gear.ScrollUpgradeableCount <= 0) return 0;
-        if (gear.Attributes._OnlyUpgrade) return 0;
-        if (gear.Type.IsMechanicGear() || gear.Type.IsDragonGear()) return 0;
-
-        int[]? data = null;
-        foreach (var item in MaxStarData)
-        {
-            if (gear.Req.Level >= item[0])
-                data = item;
-            else
-                break;
-        }
-
-        if (data == null) return 0;
-        return gear.Attributes.Superior ? data[2] : data[1];
-    }
-
-    private GearPotential?[] ConvertToGearPotentials(Wz_Node propNode)
-    {
-        GearPotential?[] potentials = [null, null, null];
-        for (var i = 0; i < propNode.Nodes.Count; i++)
-        {
-            var optionNode = propNode.Nodes[i];
             var optionCode = optionNode.Nodes["option"].GetValue<int>();
             var level = optionNode.Nodes["level"].GetValue<int>();
             var levelInfo = itemOptionDataProvider.Data.GetItemOptionLevelInfo(optionCode, level);
-            potentials[i] = new GearPotential
+            potentials.Add(new GearPotential
             {
+                Grade = optionCode / 10000,
                 Title = levelInfo.String,
                 Option = levelInfo.Option
-            };
+            });
         }
 
-        return potentials;
+        return potentials.ToArray();
     }
 
     private void HandlePropNodeDefault(Gear gear, Wz_Node propNode)
@@ -162,139 +81,10 @@ public class GearConverter(
         if (int.TryParse(propNode.Text, out _)) return;
         if (!Enum.TryParse(propNode.Text, out GearPropType propType)) return;
 
-        if (propType == GearPropType.onlyUpgrade)
-        {
-            gear.Attributes._OnlyUpgrade = true;
-            return;
-        }
-
         var value = propNode.GetValue<int>();
-        if (value == 0) return;
-
-        var optionName = propType.GetGearOptionName();
-        if (optionName != null)
+        if (value != 0)
         {
-            gear.BaseOption[optionName] = propNode.GetValue<int>();
-            return;
-        }
-
-        switch (propType)
-        {
-            // Req
-            case GearPropType.reqLevel: gear.Req.Level = value; break;
-            case GearPropType.reqSTR: gear.Req.Str = value; break;
-            case GearPropType.reqLUK: gear.Req.Luk = value; break;
-            case GearPropType.reqDEX: gear.Req.Dex = value; break;
-            case GearPropType.reqINT: gear.Req.Int = value; break;
-            case GearPropType.reqJob: gear.Req.Job = value; break;
-            case GearPropType.reqSpecJob: gear.Req.Class = value; break;
-
-            // Equip, trade
-            case GearPropType.only:
-                gear.Attributes.Only = true;
-                break;
-            case GearPropType.tradeBlock:
-                gear.Attributes.Trade = GearAttribute.GearTrade.TradeBlock;
-                break;
-            case GearPropType.equipTradeBlock:
-                if (gear.Attributes.Trade != GearAttribute.GearTrade.TradeBlock)
-                    gear.Attributes.Trade = GearAttribute.GearTrade.EquipTradeBlock;
-                break;
-            case GearPropType.onlyEquip:
-                gear.Attributes.OnlyEquip = true;
-                break;
-            case GearPropType.sharableOnce:
-                gear.Attributes._SharableOnce = true;
-                break;
-            case GearPropType.accountSharable:
-                gear.Attributes.Share = GearAttribute.GearShare.AccountSharable;
-                break;
-            case GearPropType.tradeAvailable:
-                gear.Attributes.Cuttable = (GearAttribute.GearCuttable)value;
-                break;
-            case GearPropType.CuttableCount:
-                gear.Attributes.CuttableCount = value;
-                break;
-            case GearPropType.accountShareTag:
-                gear.Attributes.AccountShareTag = true;
-                break;
-
-            // Etc
-            case GearPropType.jokerToSetItem:
-                gear.Attributes.Lucky = true;
-                break;
-            case GearPropType.bossReward:
-                gear.Attributes.BossReward = true;
-                break;
-            case GearPropType.attackSpeed:
-                gear.Attributes.AttackSpeed = value;
-                break;
-
-            // AddOption
-            case GearPropType.exUpgradeBlock:
-                gear.Attributes.CanAddOption = GearAttribute.AddOptionCan.Cannot;
-                break;
-            case GearPropType.exUpgradeChangeBlock:
-                gear.Attributes.CanAddOption = GearAttribute.AddOptionCan.Fixed;
-                break;
-
-            // Upgrade
-            case GearPropType.blockGoldHammer:
-                gear.Attributes.BlockGoldenHammer = true;
-                break;
-            case GearPropType.superiorEqp:
-                gear.Attributes.Superior = true;
-                break;
-            case GearPropType.exceptUpgrade:
-                gear.Attributes.CannotUpgrade = true;
-                break;
-            case GearPropType.tuc:
-                gear.ScrollUpgradeableCount = value;
-                break;
-
-            // Starforce
-            case GearPropType.incCHUC:
-                gear.Star = value;
-                break;
-
-            // Potential
-            case GearPropType.specialGrade:
-                gear.Attributes.SpecialGrade = true;
-                break;
-            case GearPropType.noPotential:
-                gear.Attributes.CanPotential = GearAttribute.PotentialCan.Cannot;
-                break;
-            case GearPropType.fixedPotential:
-                gear.Attributes.CanPotential = GearAttribute.PotentialCan.Fixed;
-                gear.Attributes.CanAdditionalPotential = GearAttribute.PotentialCan.Cannot;
-                break;
-            case GearPropType.tucIgnoreForPotential:
-                if (gear.Attributes.CanPotential == GearAttribute.PotentialCan.None)
-                    gear.Attributes.CanPotential = GearAttribute.PotentialCan.Can;
-                break;
-            case GearPropType.fixedGrade:
-                gear.PotentialGrade = value switch
-                {
-                    2 => PotentialGrade.Rare,
-                    3 => PotentialGrade.Epic,
-                    5 => PotentialGrade.Unique,
-                    7 => PotentialGrade.Legendary,
-                    _ => (PotentialGrade)(value - 1)
-                };
-                break;
-
-            // Attributes: incline
-            case GearPropType.charismaEXP: gear.Attributes.Incline.Charisma = value; break;
-            case GearPropType.senseEXP: gear.Attributes.Incline.Sense = value; break;
-            case GearPropType.insightEXP: gear.Attributes.Incline.Insight = value; break;
-            case GearPropType.willEXP: gear.Attributes.Incline.Will = value; break;
-            case GearPropType.craftEXP: gear.Attributes.Incline.Craft = value; break;
-            case GearPropType.charmEXP: gear.Attributes.Incline.Charm = value; break;
-
-            // Exceptional
-            case GearPropType.Etuc:
-                gear.ExceptionalUpgradeableCount = value;
-                break;
+            gear.Props[propType.ToString()] = value;
         }
     }
 }
--- WzJson/Model/Gear.cs
+++ WzJson/Model/Gear.cs
@@ -1,13 +1,14 @@
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
-using WzJson.Domain;
 
 namespace WzJson.Model;
 
 [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public class Gear
 {
-    public GearMetadata Meta { get; set; } = new();
+    public required int Id { get; set; }
 
     public required string Name { get; set; }
 
@@ -17,217 +18,20 @@ public class Gear
     [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
     public string? Desc { get; set; }
 
-    public GearType Type { get; set; }
+    public GearPotential[]? Potentials { get; set; }
 
-    public GearReq Req { get; set; } = new();
+    [JsonConverter(typeof(StringEnumConverter))]
+    [JsonExtensionData]
+    public Dictionary<string, JToken> Props { get; set; } = new();
 
-    public GearAttribute Attributes { get; set; } = new();
-
-    public GearOption BaseOption { get; set; } = new();
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int ScrollUpgradeableCount { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int MaxStar { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Star { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public PotentialGrade PotentialGrade { get; set; }
-
-    public GearPotential?[]? Potentials { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int ExceptionalUpgradeableCount { get; set; }
-
-    public bool ShouldSerializePotentials() => Potentials != null && Potentials.Any(p => p != null);
-}
-
-[JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-public class GearMetadata
-{
-    public int Id { get; set; }
-    public int Version { get; set; } = 1;
-    public object[] Add { get; set; } = [];
-}
-
-[JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-public class GearAttribute
-{
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool Only { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public GearTrade Trade { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool OnlyEquip { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public GearShare Share { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool BlockGoldenHammer { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool Superior { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int AttackSpeed { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool CannotUpgrade { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public AddOptionCan CanAddOption { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public PotentialCan CanPotential { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public PotentialCan CanAdditionalPotential { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool SpecialGrade { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int ReqLevelIncrease { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public GearCuttable Cuttable { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int CuttableCount { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool AccountShareTag { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool Lucky { get; set; }
-
-    public GearIncline Incline { get; set; } = new();
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public bool BossReward { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int GrowthExp { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int GrowthLevel { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public string? DateExpire { get; set; }
-
-    [JsonIgnore] public bool _OnlyUpgrade { get; set; }
-    [JsonIgnore] public bool _SharableOnce { get; set; }
-
-    public bool ShouldSerializeIncline() => !Incline.IsEmpty();
-
-    public enum GearTrade
-    {
-        Tradeable = 0,
-        TradeBlock = 1,
-        EquipTradeBlock = 2
-    }
-
-    public enum GearShare
-    {
-        None = 0,
-        AccountSharable = 1,
-        AccountSharableOnce = 2
-    }
-
-    public enum AddOptionCan
-    {
-        None = 0,
-        Can = 1,
-        Cannot = 2,
-        Fixed = 3,
-    }
-
-    public enum PotentialCan
-    {
-        None = 0,
-        Can = 1,
-        Cannot = 2,
-        Fixed = 3
-    }
-
-    public enum GearCuttable
-    {
-        None = 0,
-        Silver = 1,
-        Platinum = 2
-    }
-
-    [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-    public class GearIncline
-    {
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Charisma { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Insight { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Will { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Craft { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Sense { get; set; }
-
-        [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-        public int Charm { get; set; }
-
-        public bool IsEmpty()
-        {
-            return Charisma == 0 && Insight == 0 && Will == 0 && Craft == 0 && Sense == 0 && Charm == 0;
-        }
-    }
-}
-
-[JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
-public class GearReq
-{
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Level { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Str { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Luk { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Dex { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Int { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Job { get; set; }
-
-    [JsonProperty(DefaultValueHandling = DefaultValueHandling.Ignore)]
-    public int Class { get; set; }
-}
-
-public enum PotentialGrade
-{
-    Normal = 0,
-    Rare = 1,
-    Epic = 2,
-    Unique = 3,
-    Legendary = 4
+    public bool ShouldSerializePotentials() => Potentials != null;
 }
 
 [JsonObject(NamingStrategyType = typeof(CamelCaseNamingStrategy))]
 public class GearPotential
 {
+    public required int Grade { get; set; }
+    
     public required string Title { get; set; }
 
     public GearOption Option { get; set; } = new();
