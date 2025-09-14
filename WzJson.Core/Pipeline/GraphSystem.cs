f910d25d8702fcb931963d1dfc04f760ac211bba
Lechros <0onionion0@gmail.com>
2025-09-14 22:31:04 +0900

    refactor: GraphSystemRunner으로 코드 이동

--- WzJson.Core/Pipeline/GraphSystem.cs
+++ WzJson.Core/Pipeline/GraphSystem.cs
@@ -10,6 +10,11 @@ public class GraphSystem(GraphSystem.RootNode root)
         return new RootConfig(root);
     }
 
+    public void Run()
+    {
+        GraphSystemRunner.Run(root);
+    }
+
     public void Prune()
     {
         PruneNode(root);
@@ -32,75 +37,6 @@ public class GraphSystem(GraphSystem.RootNode root)
                 children.RemoveAt(i);
             }
         }
-    }
-
-    public void Run()
-    {
-        foreach (IRepositoryNode repositoryNode in root.Children)
-        {
-            IReadOnlyList<(IConverterNode, List<object>)> converterPairs = repositoryNode.Children
-                .Select(converterNode => ((IConverterNode)converterNode, new List<object>())).ToList();
-            foreach (var node in repositoryNode.Repository.EnumerateNodes())
-            {
-                foreach (var (converterNode, convertResults) in converterPairs)
-                {
-                    var convertResult = converterNode.Converter.Convert(node);
-                    if (convertResult != null)
-                    {
-                        convertResults.Add(convertResult);
-                    }
-                }
-            }
-
-            foreach (var (converterNode, convertResults) in converterPairs)
-            {
-                foreach (var childNode in converterNode.Children)
-                {
-                    switch (childNode)
-                    {
-                        case IProcessorNode processorNode: // recursive call
-                            ExecuteProcessor(processorNode, convertResults);
-                            break;
-                        case IExporterNode exporterNode:
-                            ExecuteExporter(exporterNode, convertResults);
-                            break;
-                    }
-                }
-            }
-        }
-    }
-
-    private void ExecuteProcessor(IProcessorNode processorNode, IEnumerable<object> inputs)
-    {
-        var processor = ((ProcessorNode)processorNode).Processor;
-        var outputs = EnsureCollection(processor.Process(inputs));
-        foreach (var childNode in processorNode.Children)
-        {
-            switch (childNode)
-            {
-                case IProcessorNode childProcessorNode:
-                    ExecuteProcessor(childProcessorNode, outputs);
-                    break;
-                case IExporterNode childExporterNode:
-                    ExecuteExporter(childExporterNode, outputs);
-                    break;
-            }
-        }
-    }
-
-    private void ExecuteExporter(IExporterNode exporterNode, IEnumerable<object> inputs)
-    {
-        exporterNode.Exporter.Export(inputs, exporterNode.Path);
-    }
-
-    private ICollection<T> EnsureCollection<T>(IEnumerable<T> enumerable)
-    {
-        if (enumerable is ICollection<T> collection)
-        {
-            return collection;
-        }
-
-        return enumerable.ToList();
     }
 
     public class RootNode : IGraphNode
--- WzJson.Core/Pipeline/GraphSystemRunner.cs
+++ WzJson.Core/Pipeline/GraphSystemRunner.cs
@@ -1,9 +1,73 @@
 namespace WzJson.Core.Pipeline;
 
-internal class GraphSystemRunner
+internal static class GraphSystemRunner
 {
-    class ExecutionContext
+    public static void Run(GraphSystem.RootNode root)
     {
-        
+        foreach (IRepositoryNode repositoryNode in root.Children)
+        {
+            IReadOnlyList<(IConverterNode, List<object>)> converterPairs = repositoryNode.Children
+                .Select(converterNode => ((IConverterNode)converterNode, new List<object>())).ToList();
+            foreach (var node in repositoryNode.Repository.EnumerateNodes())
+            {
+                foreach (var (converterNode, convertResults) in converterPairs)
+                {
+                    var convertResult = converterNode.Converter.Convert(node);
+                    if (convertResult != null)
+                    {
+                        convertResults.Add(convertResult);
+                    }
+                }
+            }
+
+            foreach (var (converterNode, convertResults) in converterPairs)
+            {
+                foreach (var childNode in converterNode.Children)
+                {
+                    switch (childNode)
+                    {
+                        case IProcessorNode processorNode: // recursive call
+                            ExecuteProcessor(processorNode, convertResults);
+                            break;
+                        case IExporterNode exporterNode:
+                            ExecuteExporter(exporterNode, convertResults);
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
+    private static void ExecuteProcessor(IProcessorNode processorNode, IEnumerable<object> inputs)
+    {
+        var processor = ((ProcessorNode)processorNode).Processor;
+        var outputs = EnsureCollection(processor.Process(inputs));
+        foreach (var childNode in processorNode.Children)
+        {
+            switch (childNode)
+            {
+                case IProcessorNode childProcessorNode:
+                    ExecuteProcessor(childProcessorNode, outputs);
+                    break;
+                case IExporterNode childExporterNode:
+                    ExecuteExporter(childExporterNode, outputs);
+                    break;
+            }
+        }
+    }
+
+    private static void ExecuteExporter(IExporterNode exporterNode, IEnumerable<object> inputs)
+    {
+        exporterNode.Exporter.Export(inputs, exporterNode.Path);
+    }
+
+    private static ICollection<T> EnsureCollection<T>(IEnumerable<T> enumerable)
+    {
+        if (enumerable is ICollection<T> collection)
+        {
+            return collection;
+        }
+
+        return enumerable.ToList();
     }
 }
