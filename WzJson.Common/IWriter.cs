4879d022e185a657d286e3ffbb1840cef711a6f7
Lechros <0onionion0@gmail.com>
2025-10-09 14:29:26 +0900

    chore: 사용되지 않는 프로젝트 제거

--- WzJson.Common/IWriter.cs
+++
@@ -1,9 +0,0 @@
-namespace WzJson.Common;
-
-public interface IWriter
-{
-    public bool Supports(IData data);
-    public void Write(IData data, IProgress<WriteProgressData> progress);
-}
-
-public readonly record struct WriteProgressData(int Value, int MaxValue);
--- WzJson.Common/IWzReader.cs
+++
@@ -1,8 +0,0 @@
-namespace WzJson.Common;
-
-public interface IWzReader
-{
-    public IReadOnlyList<IData> Read(IReadOptions options, IProgress<ReadProgressData> progress);
-}
-
-public readonly record struct ReadProgressData(int Value, int MaxValue);
